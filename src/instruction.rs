2c4b7949575d0f694cbdf5e7b16763b05240fd17
Vlad-Sebastian Cretu <vcretu@bitdefender.com>
2023-11-14 10:09:53 +0200

    error on wrong length

--- src/instruction.rs
+++ src/instruction.rs
@@ -68,19 +68,47 @@ impl Instruction {
                 (1, None)
             },
             0x01 | 0x04 | 0x07 | 0x0A..=0x12 | 0x1D | 0x1E | 0x21 | 0x27 | 0x7B..=0x8F | 0xB0..=0xCF => (1, None),
-            0x02 | 0x05 | 0x08 | 0x13 | 0x15 | 0x16 | 0x19 | 0x1A | 0x1C | 0x1F | 0x20 | 0x22 | 0x23 | 0x2D..=0x31 | 0x44..=0x6D | 0x90..=0xAF | 0xD0..=0xE2 | 0xFE | 0xFF => (2, None),
-            0x03 | 0x06 | 0x09 | 0x14 | 0x17 | 0x1B | 0x24..=0x26 | 0x6E..=0x72 | 0x74..=0x78 | 0xFC | 0xFD => (3, None),
+            0x02 | 0x05 | 0x08 | 0x13 | 0x15 | 0x16 | 0x19 | 0x1A | 0x1C | 0x1F | 0x20 | 0x22 | 0x23 | 0x2D..=0x31 | 0x44..=0x6D | 0x90..=0xAF | 0xD0..=0xE2 | 0xFE | 0xFF => {
+                if raw_bytecode.len() < 2 {
+                    return Err(InstructionParsingError { byte: opcode_byte, offset: offset });
+                }
+                (2, None)
+            },
+            0x03 | 0x06 | 0x09 | 0x14 | 0x17 | 0x1B | 0x24..=0x26 | 0x6E..=0x72 | 0x74..=0x78 | 0xFC | 0xFD => {
+                if raw_bytecode.len() < 3 {
+                    return Err(InstructionParsingError { byte: opcode_byte, offset: offset });
+                }
+                (3, None)
+            },
             0xFA | 0xFB => (4, None),
             0x18 => (5, None),
             0x28 => (1, Some(immediate_args as i8 as i32)),
             0x29 => (2, Some(immediate_args as i16 as i32)),
-            0x2A => (3, Some(concat_words!(raw_bytecode[1], raw_bytecode[2]) as i32)),
-            0x2B | 0x2C => (3, Some(concat_words!(raw_bytecode[1], raw_bytecode[2]) as i32)),
-            0x32..=0x3D => (2, Some(raw_bytecode[1] as i16 as i32)),
+            0x2A => {
+                if raw_bytecode.len() < 3 {
+                    return Err(InstructionParsingError { byte: opcode_byte, offset: offset });
+                }
+                (3, Some(concat_words!(raw_bytecode[1], raw_bytecode[2]) as i32))
+            },
+            0x2B | 0x2C => {
+                if raw_bytecode.len() < 3 {
+                    return Err(InstructionParsingError { byte: opcode_byte, offset: offset });
+                }
+                (3, Some(concat_words!(raw_bytecode[1], raw_bytecode[2]) as i32))
+            },
+            0x32..=0x3D => {
+                if raw_bytecode.len() < 2 {
+                    return Err(InstructionParsingError { byte: opcode_byte, offset: offset });
+                }
+                (2, Some(raw_bytecode[1] as i16 as i32))
+            },
             0x3e..=0x43 | 0x73 | 0x79..=0x7a | 0xe3..=0xf9 => {
                 return Err(InstructionParsingError { byte: opcode_byte, offset: offset });
             }
         };
+        if length > raw_bytecode.len() {
+            return Err(InstructionParsingError { byte: opcode_byte, offset: offset });
+        }
         let branch_target = match branch_target {
             Some(target) => Some((target + offset as i32) as usize),
             None => None
